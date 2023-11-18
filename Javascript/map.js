e2eb11743dfea310885ad0ed295260db04770af5
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2023-11-19 01:03:40 +0200

    add pictures and set markers draggable

---
+++ Javascript/map.js
@@ -0,0 +1,67 @@
+"use strict"
+
+// custom icons
+const base = L.icon({
+    iconUrl: "icons/home.png",
+    iconSize: [53,53],
+    popupAnchor: [1, -20]
+});
+
+const van = L.icon({
+    iconUrl: "http://localhost/Project/icons/delivery-van.png",
+    iconSize: [30,30],
+    popupAnchor: [-2, -9]
+});
+
+const person = L.icon({
+    iconUrl: "http://localhost/Project/icons/people.png",
+    iconSize: [35,35],
+    popupAnchor: [1, -20]
+});
+
+
+
+let map = L.map('map');
+
+let markers = [];
+
+map.setView([38.0, 23.85], 10);
+
+//tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map);
+
+// create markers
+
+// TODO: Create Class
+
+markers[0] = L.marker(
+    [37.98, 23.7], {
+    draggable: true,
+    icon: base
+});
+markers[1] = L.marker(
+    [38, 23.8], {
+    draggable: true,
+    icon: van
+}
+);
+
+markers[2] = L.marker(
+    [38.08, 23.87], {
+    draggable: true,
+    icon: person
+}
+);
+
+for (let marker of markers) {
+    marker.on('dragend', function (e) {
+    let pos = marker.getLatLng();
+    console.log(`${marker.getPopup().getContent()} ${pos.lat} ${pos.lng}`);
+    });
+
+    marker.addTo(map);
+}
+
+markers[1].bindPopup('ΦΟΡΤΗΓΟ 1');
+markers[2].bindPopup('ΦΟΡΤΗΓΟ 2');
+markers[0].bindPopup('ΠΡΟΙΟΝ 1');
